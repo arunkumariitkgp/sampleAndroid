aab2f3c9ddba6c14b092d1de63043472d06b1b35
Arun Kumar <aruniitkgp7@gmail.com>
2014-01-30 18:32:48 +0530

    Added splash screen activity.

--- Airwala/src/com/startupsourcing/airwala/SplashScreen.java
+++ Airwala/src/com/startupsourcing/airwala/SplashScreen.java
@@ -1,35 +1,41 @@
 package com.startupsourcing.airwala;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.Window;
 import android.view.WindowManager;
-import android.content.Intent;
-import android.graphics.Color;
-import android.view.View;
 
-public class HomeScreen extends Activity {
+public class SplashScreen extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		setContentView(R.layout.activity_home_screen);
-		View view = this.getWindow().getDecorView();
-	    view.setBackgroundColor(Color.parseColor("#2F2F2F"));
+		setContentView(R.layout.activity_splash_screen);
+		Timer t = new Timer();
+		t.schedule(loadNextActivityTask, 3000);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home_screen, menu);
+		getMenuInflater().inflate(R.menu.splash_screen, menu);
 		return true;
 	}
 
-	public void searchAirportList(View view){
-		Intent intent = new Intent(this,AirportList.class);
-		startActivity(intent);
-	}
+	private TimerTask loadNextActivityTask = new TimerTask() {
+
+		@Override
+		public void run() {
+			startActivity(new Intent(SplashScreen.this, HomeScreen.class));
+			finish();
+		}
+	};
+
 }
